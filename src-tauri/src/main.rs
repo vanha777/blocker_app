a19f990c1c6c8837eceaec79d8ecd36ea24abd38
vanha777 <vanha777@icloud.com>
2024-07-11 14:26:10 +1000

    x

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::env;
+use std::cell::RefCell;
 use std::process::{exit, Command};
+use std::sync::{Arc, Mutex};
+use std::{env, thread};
 use tauri::SystemTray;
 use tauri::{api::process::restart, Env, Manager};
 use tauri::{CustomMenuItem, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
-fn restart_application() {
-    Command::new(env::current_exe().unwrap())
-        .spawn()
-        .expect("Failed to restart application");
-    exit(0); // or exit(1) depending on your needs
-}
+// fn restart_application() {
+//     Command::new(env::current_exe().unwrap())
+//         .spawn()
+//         .expect("Failed to restart application");
+//     exit(0); // or exit(1) depending on your needs
+// }
+
 fn main() {
-    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    println!("Debug: Starting Applcation ...");
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     // let hide = CustomMenuItem::new("hide".to_string(), "Hide");
     let open = CustomMenuItem::new("open".to_string(), "Open");
@@ -25,13 +28,27 @@ fn main() {
         .add_item(quit);
     let tray = SystemTray::new().with_menu(tray_menu);
 
-    // Set up a panic hook to catch panics and restart the application
-    std::panic::set_hook(Box::new(|panic_info| {
-        println!("Application panicked: {:?}", panic_info);
-        env::set_var("TAURI_APP_CRASHED", "true");
+    // Mutex to control restart synchronization
+    let restart_mutex = Arc::new(Mutex::new(RefCell::new(())));
+
+    // Set custom panic hook
+    std::panic::set_hook(Box::new({
+        let restart_mutex = restart_mutex.clone();
+        move |panic_info| {
+            println!("Debug: Application panicked: {:?}", panic_info);
+            restart_application(&restart_mutex);
+        }
     }));
 
     tauri::Builder::default()
+        .setup(|app| {
+            // if conditon updates detect
+            // ex:
+            if check_update() {
+                restart(&app.env());
+            }
+            Ok(())
+        })
         .system_tray(tray)
         .on_system_tray_event(|app, event| match event {
             SystemTrayEvent::LeftClick {
@@ -81,27 +98,51 @@ fn main() {
             }
             _ => {}
         })
-        // set up restart apps with path when crashes
         // .setup(|app| {
-        //     restart(&app.env());
+        //     if env::var("TAURI_APP_CRASHED").is_ok() {
+        //         env::remove_var("TAURI_APP_CRASHED");
+        //         println!("Restarting application after crash...");
+        //         restart_application();
+        //     }
         //     Ok(())
         // })
-        .setup(|app| {
-            if env::var("TAURI_APP_CRASHED").is_ok() {
-                env::remove_var("TAURI_APP_CRASHED");
-                println!("Restarting application after crash...");
-                restart_application();
-            }
-            Ok(())
-        })
         .build(tauri::generate_context!())
         .expect("error while running tauri application")
         .run(|_app_handle, event| match event {
             tauri::RunEvent::ExitRequested { api, .. } => {
                 api.prevent_exit();
             }
-            _ => {}
+            tauri::RunEvent::Exit => {
+                _app_handle.restart();
+            }
+            // tauri::RunEvent::WindowEvent { label, event } => todo!(),
+            // tauri::RunEvent::Ready => todo!(),
+            // tauri::RunEvent::Resumed => todo!(),
+            // tauri::RunEvent::MainEventsCleared => todo!(),
+            _ => {
+                // _app_handle.restart();
+                // println!("Debug: Application is running");
+            }
         });
+    println!("Debug: Application is running");
+}
+
+fn restart_application(restart_mutex: &Arc<Mutex<RefCell<()>>>) {
+    // Lock the mutex to synchronize restart
+    let _lock = restart_mutex.lock().unwrap();
+    // Spawn a new thread to handle the restart
+    thread::spawn(move || {
+        // Wait for the main thread to exit gracefully
+        // Add any necessary cleanup logic here
+
+        // Restart the application
+        Command::new(env::current_exe().unwrap())
+            .spawn()
+            .expect("Failed to restart application");
+        exit(0); // or exit(1) depending on your needs
+    });
+    // exit gracegully
+    std::process::exit(0);
 }
 
 #[tauri::command]
@@ -110,7 +151,15 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
+fn check_update() -> bool {
+    // updating config file ......
+    println!("Debug: Sending config files to cloud and check for update ...");
+    false
+}
+
+#[tauri::command]
 fn crash() -> String {
-    let parsed_finger = "finger".parse::<i32>().unwrap(); // Attempt to parse and immediately unwrap
-    format!("Parsed finger: {}", parsed_finger)
+    // let parsed_finger = "finger".parse::<i32>().unwrap(); // Attempt to parse and immediately unwrap
+    // format!("Parsed finger: {}", parsed_finger)
+    panic!("Normal panic");
 }
