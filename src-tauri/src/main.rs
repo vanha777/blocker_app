874f93f4e50e26027237f3179f197240bf0829f5
vanha777 <vanha777@icloud.com>
2024-07-15 01:29:01 +1000

    add reading config when start up,create default config in os system

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,39 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
 use std::cell::RefCell;
+use std::collections::HashMap;
+use std::fs::{create_dir_all, File};
+use std::io::Read;
+use std::path::PathBuf;
 use std::process::{exit, Command};
 use std::sync::{Arc, Mutex};
 use std::{env, thread};
+use tauri::api::path::config_dir;
 use tauri::SystemTray;
 use tauri::{api::process::restart, Env, Manager};
 use tauri::{CustomMenuItem, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 use tauri_plugin_autostart::MacosLauncher;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Config {
+    // this is a token from cloud -> required for cloud http request
+    session_id: Option<String>,
+    cloud_url: Option<String>,
+    client_id: Option<String>,
+    version: Option<u8>,
+    api_config: Option<HashMap<String, Vec<ApiConfig>>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ApiConfig {
+    url: Option<String>,
+    method: Option<String>,
+    data: Option<String>,
+    active: Option<bool>,
+}
 // fn restart_application() {
 //     Command::new(env::current_exe().unwrap())
 //         .spawn()
@@ -47,10 +73,59 @@ fn main() {
             Some(vec!["--flag1", "--flag2"]), /* arbitrary number of args to pass to your app */
         ))
         .setup(|app| {
-            // if conditon updates detect
-            // ex:
-            if check_update() {
-                restart(&app.env());
+            println!("Debug: Setting up application ...");
+            let mut config = Config::default();
+            let app_name = "com.strong-extractions.dev";
+            let config_dir = config_dir()
+                .expect("Failed to get config directory")
+                .join(app_name);
+            println!("app.env {:?}", config_dir);
+            // Create config_dir if not already exist
+            create_dir_all(&config_dir).expect("Failed to create config directory");
+            let config_file_path = config_dir.join("config.json");
+            // Check if the config file exists, if not create a default one
+            match !config_file_path.exists() {
+                true => {
+                    println!("Debug: No config file found ...");
+                    let default_config = Config {
+                        session_id: None,
+                        cloud_url: None,
+                        client_id: Some(Uuid::new_v4().to_string()),
+                        version: Some(0),
+                        api_config: None,
+                    };
+                    println!("Debug: Creating default config file...");
+                    let config_content = serde_json::to_string_pretty(&default_config)
+                        .expect("Failed to serialize default config");
+                    std::fs::write(&config_file_path, config_content)
+                        .expect("Failed to write default config file");
+                    config = default_config;
+                }
+                false => {
+                    println!("Debug: Reading config file...");
+                    // Read the config file
+                    let mut file = File::open(&config_file_path).map_err(|e| {
+                        eprintln!("Failed to open config file: {:?}", e);
+                        e
+                    })?;
+
+                    let mut contents = String::new();
+
+                    file.read_to_string(&mut contents).map_err(|e| {
+                        eprintln!("Failed to read config file: {:?}", e);
+                        e
+                    })?;
+
+                    let existing_config: Config = from_str(&contents).map_err(|e| {
+                        eprintln!("Failed to parse config file: {:?}", e);
+                        e
+                    })?;
+                    config = existing_config;
+                    // check 4 updates
+                    if check_update(config_file_path, config) {
+                        restart(&app.env());
+                    }
+                }
             }
             Ok(())
         })
@@ -156,10 +231,20 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn check_update() -> bool {
+fn check_update(config_dir: PathBuf, config: Config) -> bool {
     // updating config file ......
     println!("Debug: Sending config files to cloud and check for update ...");
+    println!("Debug: No update required ...");
+    config_update();
     false
+}
+
+#[tauri::command]
+fn config_update() -> Result<i32, String> {
+    // updating config file ......
+    println!("Debug: Downloading config file ...");
+    println!("Debug: Parsing config file ...");
+    Ok(200)
 }
 
 #[tauri::command]
