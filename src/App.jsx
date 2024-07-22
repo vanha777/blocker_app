8a94b193cada7ebb3730da11be00aecdc982f1e5
vanha777 <vanha777@icloud.com>
2024-07-22 21:41:36 +1000

    x

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
   // };
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div data-theme="cupcake" className="flex flex-col min-h-screen">
 
 
       {loading &&
